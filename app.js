718ca75f92b17de728bbb97a152113e59be43cba
panjun0124 <panjun0124@gmail.com>
2026-02-05 10:19:07 +0900

    Implement CORS middleware in app.js
    Added CORS headers to allow cross-origin requests.

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+// CORS 헤더 추가
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 
 app.post('/chat', async (req, res) => {
